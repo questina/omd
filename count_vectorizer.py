30afa34c42583caed13f4b9060504cf733569cfe
questina <chrismatis123@gmail.com>
2023-10-18 11:40:24 +0200

    added tests

--- count_vectorizer.py
+++ count_vectorizer.py
@@ -1,4 +1,5 @@
-from typing import Iterable, List
+from collections import defaultdict
+from typing import Iterable, List, Optional
 
 
 class CountVectorizer:
@@ -20,16 +21,14 @@ class CountVectorizer:
     """
 
     def __init__(
-            self,
-            lowercase: bool = True,
-            stop_words: Iterable[str] = None
+        self,
+        lowercase: bool = True,
+        stop_words: Optional[Iterable[str]] = None,
     ) -> None:
-        if stop_words is None:
-            stop_words = {}
 
         self._word_to_idx = None
         self.lowercase = lowercase
-        self.stop_words = stop_words
+        self.stop_words = stop_words or {}
 
     def _text_preprocessor(self, text: str) -> List[str]:
         """
@@ -47,34 +46,32 @@ class CountVectorizer:
         ]
         return preprocessed_text
 
-    def fit_transform(self, corpus: Iterable[str]) -> List[int]:
+    def fit_transform(self, corpus: Iterable[str]) -> List[List[int]]:
         """
         Learn vocabulary of words and convert corpus to document-term matrix.
 
         :param corpus: collection of texts to tokenization.
         :return: processed texts in a form of document-term matrix.
         """
-        self._word_to_idx = {}
+        self._word_to_idx = defaultdict()
+        self._word_to_idx.default_factory = self._word_to_idx.__len__
         corpus_word_freq = []
 
         for text in corpus:
-            text_word_freq = {}
+            text_word_freq = defaultdict(int)
             for word in self._text_preprocessor(text):
-                self._word_to_idx[word] = self._word_to_idx.get(
-                    word, len(self._word_to_idx)
-                )
                 word_idx = self._word_to_idx[word]
-                text_word_freq[word_idx] = text_word_freq.get(word_idx, 0) + 1
+                text_word_freq[word_idx] += 1
             corpus_word_freq.append(text_word_freq)
 
         count_matrix = [
-            [0 for _ in range(len(self._word_to_idx))]
+            [0] * len(self._word_to_idx)
             for _ in range(len(corpus))
         ]
 
-        for i, text_word_freq in enumerate(corpus_word_freq):
-            for word_idx in text_word_freq:
-                count_matrix[i][word_idx] = text_word_freq[word_idx]
+        for count_vector, text_word_freq in zip(count_matrix, corpus_word_freq):
+            for word_idx, word_freq in text_word_freq.items():
+                count_vector[word_idx] = word_freq
 
         return count_matrix
 
@@ -88,7 +85,7 @@ class CountVectorizer:
         if self._word_to_idx is None:
             raise Exception('Vocabulary not fitted yet')
         return [
-            t for t, i in sorted(
+            token for token, idx in sorted(
                 self._word_to_idx.items(),
                 key=lambda dict_item: dict_item[1]
             )
@@ -96,8 +93,10 @@ class CountVectorizer:
 
 
 if __name__ == '__main__':
-    corpus = ['Crock Pot Pasta Never boil pasta again',
-              'Pasta Pomodoro Fresh ingredients Parmesan to taste']
+    corpus = [
+        'Crock Pot Pasta Never boil pasta again',
+        'Pasta Pomodoro Fresh ingredients Parmesan to taste',
+    ]
     vectorizer = CountVectorizer()
     count_matrix = vectorizer.fit_transform(corpus)
     print(vectorizer.get_feature_names())
